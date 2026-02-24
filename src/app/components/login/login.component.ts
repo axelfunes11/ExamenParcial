01fdba1a0b93b4756ae59dbbdb2b000b17f01753
Axel Funes <202201160@estudiante.udv.edu.gt>
2026-02-23 18:30:46 -0600

    PROYECTO DE PARCIAL 1 CORRIENDO CON EXITO

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  styleUrls: ['./login.component.scss'],
+  template: `
+    <button (click)="login()">Login</button>
+  `
+})
+export class LoginComponent {
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login('admin', '1234')
+      .subscribe(res => console.log(res));
+  }
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  login(username: string, password: string) {
+    return of({ success: username === 'admin' && password === '1234' });
+  }
+
+}
