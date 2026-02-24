2a8673c24b7bbae9bb33e00691b46d78719cb475
Dev A <deva@ejemplo.com>
2026-02-23 19:28:14 -0600

    [UPDATE] Login con nuevo teamplate

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,16 +5,19 @@ import { AuthService } from 'src/app/services/auth.service';
   selector: 'app-login',
   styleUrls: ['./login.component.scss'],
   template: `
-    <button (click)="login()">Login</button>
+    <h1>Login</h1>
+  <input placeholder="Usuario" #user>
+  <input type="password" placeholder="Password" #pass>
+  <button (click)="login(user.value, pass.value)">Login</button>
   `
 })
 export class LoginComponent {
 
   constructor(private authService: AuthService) {}
 
-  login() {
-    this.authService.login('admin', '1234')
-      .subscribe(res => console.log(res));
-  }
+  login(username: string, password: string) {
+  this.authService.login(username, password)
+    .subscribe(res => console.log('UI', res));
+}
 
 }
